721a8e2fcd614240948b7a279167e9b98dcf0ed6
David Andersson <david.andersson@zocom.se>
2021-09-10 14:06:50 +0200

    Add one script

---
+++ src/scripts/addOneDocument.js
@@ -0,0 +1,19 @@
+const { connect } = require('../database.js')
+const db = connect()
+
+const USERS = 'users'
+
+addOne();
+
+
+async function addOne() {
+	console.log('Add a new document...');
+	const object = {
+		name: 'Hermione Granger',
+		email: 'hermione@email.com',
+		lastOnline: 0
+	}
+
+	const docRef = await db.collection(USERS).add(object)
+	console.log('Added document with the id ' + docRef.id);
+}
